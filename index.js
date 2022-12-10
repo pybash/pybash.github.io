7c0f4c3c2dd83a74f101a55b4c620e9d9696d16e
pybash <pybash@github.com>
2022-12-09 22:57:14 -0500

    timer update

--- index.js
+++ index.js
@@ -17,8 +17,20 @@ document.addEventListener('DOMContentLoaded', () => {
         now = moment()
         duration = moment.duration(decision.diff(now));
         daysText.innerText = Math.round(duration.asDays() + 1)
-        hoursText.innerText = Math.round(duration.hours())
-        minutesText.innerText = Math.round(duration.minutes())
-        secondsText.innerText = Math.round(duration.seconds())
+        if(Math.round(duration.hours()) < 10) {
+            hoursText.innerText = "0" + Math.round(duration.hours())
+        } else {
+            hoursText.innerText = Math.round(duration.hours())
+        }
+        if( Math.round(duration.minutes()) < 10) {
+            minutesText.innerText = "0" + Math.round(duration.minutes())
+        } else {
+            minutesText.innerText = Math.round(duration.minutes())
+        }
+        if(Math.round(duration.seconds()) < 10) {
+            secondsText.innerText = "0" + Math.round(duration.seconds())
+        } else {
+            secondsText.innerText = Math.round(duration.seconds())
+        }
     }, 100)
 })
